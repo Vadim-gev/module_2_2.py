486771983c824b9bf0d1f36dc3847e82067e6bab
Vadim-gev <vadim_gevorkian@mail.ru>
2024-07-10 11:51:53 +0300

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,9 @@
+first = 35
+second = 36
+third = 35
+if first == second and first == third and second == third:
+    print(3)
+if first == second or first == third or second == third:
+    print(2)
+if first != second and first != third and second == third:
+    print(0)
